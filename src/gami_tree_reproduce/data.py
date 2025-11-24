add7923c4f9c6a34eee7357fdcc501226603c94e
mms1410 <svenmauricemorlock@yahoo.com>
2025-11-24 10:39:20 +0100

    :wrench:  ruff ignore ISC001

--- src/gami_tree_reproduce/data.py
+++ src/gami_tree_reproduce/data.py
@@ -4,13 +4,13 @@ import yaml
 from sklearn.datasets import fetch_openml
 
 root = Path.cwd()
-path_config = root / "conf" / "data.yaml"
+path_config = root / "conf" / "config.yaml"
 with Path.open(path_config) as f:
     cfg = yaml.safe_load(f)
-path_data = cfg["path_data"]
+path_data = Path(cfg["path_data"])
 
 
-def download_data_openml(id: int) -> Path:
+def download_data_openml(id: int, file_name: str) -> Path:
     destination = root / path_data
     if not Path.is_dir(destination):
         msg = f"{destination} is not an existing directory."
@@ -19,7 +19,18 @@ def download_data_openml(id: int) -> Path:
     data = fetch_openml(data_id=id, as_frame=True, parser="auto")
     frame = data.frame
 
-    data_name = data.details["name"]
-    full_path = destination / Path(f"{data_name}.csv")
+    full_path = destination / Path(f"{file_name}.csv")
     frame.to_csv(full_path)
     return full_path
+
+def get_data_from_conf(conf_data_folder:Path = root / "conf" / "datasets") -> None:
+    for config_file in conf_data_folder.rglob("*.yaml"):
+        with Path.open(config_file) as f:
+            cfg  =yaml.safe_load(f)
+        if "openml_id" in cfg:
+            id = cfg["openml_id"]
+            file_name = cfg["name"]
+            download_data_openml(id, file_name)
+
+path_data.mkdir()
+get_data_from_conf()
