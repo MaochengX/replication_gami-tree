6f5fac3f438f4def4233bf2ae97484a5064067d0
mms1410 <svenmauricemorlock@yahoo.com>
2025-11-28 15:21:02 +0100

    :construction: work on models

--- src/gami_tree_reproduce/models.py
+++ src/gami_tree_reproduce/models.py
@@ -1,127 +1,163 @@
-from dataclasses import asdict, dataclass
-from typing import Any, ClassVar, Literal, Protocol, TypeVar
+from abc import abstractmethod
+from collections.abc import Iterable
+from dataclasses import asdict
+from typing import (
+    Any,
+    ClassVar,
+    Literal,
+    Protocol,
+    override,
+    runtime_checkable,
+)
 
+import numpy as np
 from interpret.glassbox import ExplainableBoostingClassifier as EBMC
 from interpret.glassbox import ExplainableBoostingRegressor as EBMR
 from xgboost import XGBClassifier as XGBC
 from xgboost import XGBRegressor as XGBR
 
-
-# forward declaration, later to be imported
-class GAMITR: ...  # for regression task
+from src.gami_tree_reproduce.params import EBMParams, GAMITParams, Params, XGBParams
 
 
-class GAMITC: ...  # for classification task
+# TODO: Forward declarations for now until implemented
+class GAMITR: ...
 
 
-@dataclass
-class EBMParams:
-    """ """
+class GAMITC: ...
 
 
-@dataclass
-class XGBParams:
-    """ """
+# Protocol Definitions
 
 
-@dataclass
-class GAMITParams:
-    """
-    GAMIT = GAMI-Tree
-    """
-
-
+@runtime_checkable
 class Classifier(Protocol):
-    """
-    Protocol for Classifier variables.
-    """
-
-    def train(self):
-        pass
-
-    def predict_proba(self):
-        pass
+    def train(self, X, y) -> None: ...
+    def predict_proba(self, X) -> np.ndarray | Iterable[float]: ...
 
 
+@runtime_checkable
 class Regressor(Protocol):
-    """
-    Protocol for Regressor variables.
-    """
-
-    def train(self):
-        pass
-
-    def predict(self):
-        pass
+    def train(self, X, y) -> None: ...
+    def predict(self, X) -> np.ndarray | Iterable[float]: ...
 
 
-Params = TypeVar("Params", bound=EBMParams | XGBParams | GAMITParams)
 Model = Classifier | Regressor
 
 
+####################
+# Registry Metaclass
+####################
 class InducerRegistryBase(type):
     """
-    Metaclass for registration of Inducer classes.
+    Metaclass automatically registering Inducer subclasses.
     """
 
-    inducer_registry: ClassVar[dict[str | type["BaseInducerClass"]]] = {}
+    inducer_registry: ClassVar[dict[str, type["BaseInducerClass"]]] = {}
 
+    @override
     def __new__(cls, name, bases, attrs):
-        """
-        Register a new Inducer at definition time here.
-        """
         new_cls = super().__new__(cls, name, bases, attrs)
         if name != "BaseInducerClass":
-            cls.inducer_registry[new_cls.__name__] = new_cls
+            cls.inducer_registry[name] = new_cls
         return new_cls
 
     @classmethod
-    def get_registry(cls) -> dict:
-        """Return (copy of) inducer registry."""
+    def get_registry(cls) -> dict[str, type["BaseInducerClass"]]:
         return dict(cls.inducer_registry)
 
 
+##############
+# Base Inducer
+##############
 class BaseInducerClass(metaclass=InducerRegistryBase):
-    """ """
+    """
+    Base class for model inducers.
+    """
 
-    def __init__(self, params: Params):
-        """ """
-        self.params = params
-        self._model: Model | None = None
+    @property
+    @abstractmethod
+    def classifier_class(self) -> type[Classifier]: ...
 
-    def train(self, X: Any, Y: Any) -> Model:
-        """ """
-        raise NotImplementedError
+    @property
+    @abstractmethod
+    def regressor_class(self) -> type[Regressor]: ...
 
-    def instantiate(self, task: Literal["classification", "regression"]) -> Model:
-        """
-        Instantiae untrained model.
-        """
-        is_classification = task == "classification"
+    def instantiate(
+        self,
+        task: Literal["classification", "regression"],
+        params: Params | dict | None = None,
+    ) -> Model:
         model_class = (
-            self.classifier_class if is_classification else self.regressor_class
+            self.classifier_class if task == "classification" else self.regressor_class
         )
 
-        if model_class is None:
-            msg = f"{self.__class__.__name__} does not have a classifier or regressor"
-            raise ValueError(msg)
+        # Convert dataclass parameters to dict
+        if params is None:
+            params_dict = {}
+        elif hasattr(params, "__dataclass_fields__"):
+            params_dict = asdict(params)
+        else:
+            params_dict = dict(params)
 
-        params_dict = (
-            asdict(self.params) if hasattr(self.params, "__dataclass_fields__") else {}
-        )
+        self._params = params_dict
+
+        # Return a NEW INSTANCE EVERY TIME
         return model_class(**params_dict)
 
+    def train(self, X: Any, y: Any, params: Params | dict) -> np.ndarray:
+        """ " """
+        if self._trained:
+            msg = "Cannot retrain an already trained model."
+            raise AssertionError(msg)
 
+        self._trained = True
+        self._params = params
+
+        self._model.train(X, y)
+
+
+###################
+# Concrete Inducers
+###################
 class EBMinducer(BaseInducerClass):
-    classifier_class = EBMC
-    regressor_class = EBMR
+    param_class = EBMParams
+
+    @property
+    def classifier_class(self) -> type[EBMC]:
+        return EBMC
+
+    @property
+    def regressor_class(self) -> type[EBMR]:
+        return EBMR
 
 
 class XGBinducer(BaseInducerClass):
-    classifier_class = XGBC
-    regressor_class = XGBR
+    param_class = XGBParams
+
+    @property
+    def classifier_class(self) -> type[XGBC]:
+        return XGBC
+
+    @property
+    def regressor_class(self) -> type[XGBR]:
+        return XGBR
 
 
 class GAMITinducer(BaseInducerClass):
-    classifier_class = GAMITC
-    regressor_class = GAMITR
+    param_class = GAMITParams
+
+    @property
+    def classifier_class(self) -> type[GAMITC]:
+        return GAMITC
+
+    @property
+    def regressor_class(self) -> type[GAMITR]:
+        return GAMITR
+
+
+# Registry Access
+
+inducer_registry = InducerRegistryBase.get_registry()
+ebm_base = EBMinducer()
+xgb_base = XGBinducer()
+gamit_base = GAMITinducer()
