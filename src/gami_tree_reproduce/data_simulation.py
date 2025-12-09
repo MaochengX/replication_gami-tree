525dfc2d17a793f5ff6bec79d59c962c30adeead
mms1410 <svenmauricemorlock@yahoo.com>
2025-12-09 18:03:39 +0100

    :recycle: add commandline overwrite and config asset

--- src/gami_tree_reproduce/data_simulation.py
+++ src/gami_tree_reproduce/data_simulation.py
@@ -1,3 +1,5 @@
+import argparse
+import sys
 from pathlib import Path
 
 import numpy as np
@@ -5,15 +7,20 @@ import pandas as pd
 from omegaconf import OmegaConf
 
 ROOT = Path.cwd()
+DATA = ROOT / "data"
+ASSET = ROOT / "assets" / "conf" / "data"
+SIMULATION_CONF = ROOT / "conf" / "data" / "simulation.yaml"
+
+DATA.mkdir(exist_ok=True, parents=True)
+ASSET.mkdir(exist_ok=True, parents=True)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--filenameprefix", type=str, default="")
+args, unknown = parser.parse_known_args()
+filenameprefix = args.filenameprefix
 
 
-def get_model1(X) -> np.ndarray:
-    """
-    g(x_i) = \sum_{j=1}^5 x_{ij} +
-             \sum_{j=6}^{8} 0.5 x_{ij}^2 +
-             \sum_{j=9}^10 x_{ij}\mathbb{I}(x_{ij}>0) +
-             \sum_{j=1}^10 \sum_{k=j+1}^10 0.2 x_{ij} x_{ik}
-    """
+def model1(X) -> np.ndarray:
     term1 = X[:, 0:5].sum(axis=1)
 
     term2 = 0.5 * np.sum(X[:, 5:8] ** 2, axis=1)
@@ -26,20 +33,31 @@ def get_model1(X) -> np.ndarray:
     return term1 + term2 + term3 + term4
 
 
-def get_model2(covariate): ...
+def model2(covariate): ...
 
 
-def get_model3(covariates): ...
+def model3(covariates): ...
 
 
-def get_model4(covariates): ...
+def model4(covariates): ...
 
 
-def write_data(covariates, y, filename, folderpath=ROOT / "data") -> None:
+def write_data(
+    cfg, covariates, modelfunc, prefix=filenameprefix, folderpath=DATA
+) -> None:
+    y = modelfunc(covariates)
+    filename_data = prefix + modelfunc.__name__ + ".csv"
+    filename_asset = prefix + modelfunc.__name__ + ".yaml"
+
     cols = [f"X_{i + 1}" for i in range(covariates.shape[1])]
     data = pd.DataFrame(covariates, columns=cols)
     data["y"] = y
-    data.to_csv(Path(folderpath, filename), index=False)
+    data.to_csv(Path(folderpath, filename_data), index=False)
+    write_configuration(cfg, filename_asset)
+
+
+def write_configuration(config, filename, folderpath=ASSET):
+    OmegaConf.save(config, Path(folderpath, filename))
 
 
 def numpy_resolver(func_name, *args):
@@ -61,7 +79,13 @@ OmegaConf.register_new_resolver(
 )
 
 
-cfg = OmegaConf.load(ROOT / "conf" / "data" / "simulation.yaml")
+cfg = OmegaConf.load(SIMULATION_CONF)
+
+
+# merge command line overrides: key=value pairs
+cli_cfg = OmegaConf.from_dotlist(sys.argv[1:])
+cfg = OmegaConf.merge(cfg, cli_cfg)
+
 rng = np.random.default_rng(cfg.SEED)
 
 
@@ -73,5 +97,4 @@ X2 = generator2(**cfg.x2_generator.params)
 
 covariates = np.hstack([X1[:, :10], X2])
 
-y = get_model1(covariates)
-write_data(covariates, y, "simulation_1.csv")
+write_data(cfg, covariates, model1)
