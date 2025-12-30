1021a4c99703c4bc9f83455cdd530d6489fb0602
mms1410 <svenmauricemorlock@yahoo.com>
2025-12-30 13:47:36 +0100

    :construction: implemented experiment-loop skeleton

--- src/gami_tree_reproduce/main.py
+++ src/gami_tree_reproduce/main.py
@@ -1,36 +1,68 @@
-import argparse
 from itertools import product
 from pathlib import Path
 
-from omegaconf import OmegaConf
+import pandas as pd
+from omegaconf import DictConfig, OmegaConf
+
+from gami_tree_reproduce.model.inducers import get_inducer_class
+from gami_tree_reproduce.model.params import get_parameter_class
 
 
-def get_parameter_grid(cfg_params: OmegaConf, section_name="fix") -> None:
-    if section_name in cfg_params:
-        params_experiment = cfg_params[section_name]
-        parameter_experiment_grid = [
-            dict(zip(params_experiment, value, strict=True))
-            for value in product(*params_experiment.values())
-        ]
-    else:
-        msg = f"Expected to find '{section_name}' section in config but found none."
-        raise KeyError(msg)
-
-    return parameter_experiment_grid
-
-
-def get_filename_path(filename: str, dir_walk: list[Path]) -> Path:
+def param_config_to_grid(cfg_params: OmegaConf, no_combo="tune") -> list[dict]:
     """
-    _summary_
+    Create a list of all possible parameter configurations.
 
     Args:
-        filename (str): _description_
-        walk_list (list[Path]): _description_
+        cfg_params (OmegaConf): Configuration from yaml read into OmegaConf.
+        no_combo (str, optional): Value for keys that are excluded in calculation of all grid-combinations. Defaults to "tune".
 
     Returns:
-        Path: _description_
+        list[dict]: A list of dictionaries, where each dictionary corresponds to a certain configuration of parameters.
+
+    Example:
+        > param_config_to_grid({"alpha": [1,2,3], "beta": [1, 2], "gamma": "tune", "delata": 1})
+        > [{"alpha": 1, "beta": 1, "gamma": "tune", "delata": 1}, {"alpha": 2, "beta": 1, "gamma": "tune", "delata": 1},
+             {"alpha": 3, "beta": 1, "gamma": "tune", "delata": 1}, {"alpha": 1, "beta": 2, "gamma": "tune", "delata": 1},
+             {"alpha": 2, "beta": 2, "gamma": "tune", "delata": 1}, {"alpha": 3, "beta": 2, "gamma": "tune", "delata": 1}]
     """
-    return next(p for p in dir_walk if p.stem == filename)
+    if isinstance(cfg_params, DictConfig):
+        cfg_params = OmegaConf.to_container(cfg_params)
+
+    # tunable hyperparameters are just added in the end to each combination
+    hyperparameter_dict = {}
+    if no_combo in cfg_params:
+        hyperparameter_dict = dict.fromkeys(cfg_params[no_combo], no_combo)
+        cfg_params.pop("tune")
+    param_grid = {
+        k: [v] if not isinstance(v, list) else v for k, v in cfg_params.items()
+    }  # convert to list (iterable) with possibly single element
+    # create the actual grid ad add hyperaprameters
+    param_grid = [
+        dict(zip(param_grid.keys(), combination, strict=True))
+        for combination in product(*param_grid.values())
+    ]
+
+    return [{**d, **hyperparameter_dict} for d in param_grid]
+
+
+def get_inducer_dictionary_grid(inducers: list[Path]) -> dict:
+    """
+    Create configuration grids for all inducers for which there is a configuration file.
+
+    Args:
+        inducers (list[Path]): List of all paths to configurations.
+
+    Returns:
+        dict: Dictionary with inducer name as key and a dictionary of possible configurations as value.
+    """
+    total_grid = {}
+    for inducer_yaml in inducers:
+        inducer_name = inducer_yaml.stem
+        inducer_cfg = OmegaConf.load(inducer_yaml)
+        inducer_grid = param_config_to_grid(inducer_cfg.parameters)
+        total_grid[inducer_name] = inducer_grid
+
+    return total_grid
 
 
 def add_list_to_grid(key_name: str, value_list: list, grid: list[dict]) -> list[dict]:
@@ -55,27 +87,44 @@ def add_list_to_grid(key_name: str, value_list: list, grid: list[dict]) -> list[
     return [{**config, f"{key_name}": value} for config in grid for value in value_list]
 
 
+def do_experiment(): ...
+
+
 ROOT = Path.cwd()
 CONF = ROOT / "conf"
 DATA = ROOT / "data"
 ASSETS = ROOT / "assets"
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--inducer", type=str, default="all")
-parser.add_argument("--data", type=str, default="all")
-parser.add_argument("--param_fix", type=str, default="")
-parser.add_argument("--param_tune", type=str, default="")
-args, unknown = parser.parse_known_args()
 
-arg_data = args.data
-arg_inducer = args.inducer
+datasets_paths = list(DATA.glob("*.pq"))
+configs_inducer = list(Path(CONF / "inducer").glob("*.yaml"))
 
+inducers_dictionary_grid = get_inducer_dictionary_grid(configs_inducer)
 
-datasets = list(DATA.glob("*.pq"))
-dataset_names = [p.stem for p in datasets]
-inducer = "ebm"
-path_conf_inducer = CONF / "inducer" / Path(inducer).with_suffix(".yaml")
-cfg_inducer = OmegaConf.load(path_conf_inducer)
-params = cfg_inducer.parameters
-inducer_experiment_grid = get_parameter_grid(params)
-# TODO: iterate over grid
+for dataset_path in datasets_paths:
+    # dataset names are stores as parquet files and end in 'c' or 'r' for categorization/regression tasks
+    experiment_name = dataset_path.stem
+    if experiment_name[-1] == "c":
+        task = "classification"
+    else:
+        taks = "regression"
+
+    # get class and instantiate parameter and inducer objects
+    for inducer_name in inducers_dictionary_grid:
+        configurations = inducers_dictionary_grid[inducer_name]
+        for current_configuration in configurations:
+            hpo_params = {
+                key: current_configuration.pop(key)
+                for key, value in list(current_configuration.items())
+                if value == "tune"
+            }
+            hpo_params = hpo_params.keys()
+
+            params = get_parameter_class(inducer_name)(
+                params=current_configuration, task=task
+            )
+            inducer = get_inducer_class(inducer_name)(taske=task, params_wrapper=params)
+
+            # TODO: train hpo_params (set to default in params)
+            data = pd.read_parquet(dataset_path)
+            # TODO: train model on dataset
